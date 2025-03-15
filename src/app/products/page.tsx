26f2877551dffc899aac0b1124c6eb81518822bb
Yaseen Ahmad <yaseenahmad02.777@gmail.com>
2025-03-15 23:26:04 +0530

    middleware test

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Products from '../_components/Products'
+
+const page = () => {
+  return (
+    <div><Products></Products></div>
+  )
+}
+
+export default page
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,13 @@ import { NextResponse } from "next/server";
 export default withAuth(function middleware(req) {
     const token = req.nextauth?.token;
 
-    if (req.nextUrl.pathname.startsWith("/admin") && token?.role !== "admin") {
-        return NextResponse.redirect(new URL("/", req.url));
-    }
+    // if (req.nextUrl.pathname.startsWith("/admin") && token?.role !== "admin") {
+    //     return NextResponse.redirect(new URL("/", req.url));
+    // }
 
     return NextResponse.next();
 });
 
 export const config = {
-    matcher: ["/admin/:path*"], 
+    // matcher: ["/admin/:path*"], 
 };
